211a0573457eeada5a1d03d2d97f1fad86947a95
Evgeniy Naumov <63817948+evgenii020304@users.noreply.github.com>
2023-05-15 01:27:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,23 +5,23 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
+BST<std::string> makeTree(char* filename) {
   std::string word;
-  std::ifstream text(filename);
+  std::ifstream file(filename);
   BST<std::string>* tree = new BST<std::string>;
   char str = ' ';
-  while (!text.eof()) {
-    while (str < 65 && (!text.eof())) {
-      text.get(str);
+  while (!file.eof()) {
+    while (str < 65 && (!file.eof())) {
+      file.get(str);
     }
-    while (str >= 65 && (!text.eof())) {
+    while (str >= 65 && (!file.eof())) {
       if (str >= 65 && str <= 90) {
         word += str;
       }
       if (str >= 97 && str <= 122) {
         word += str;
       }
-      text.get(str);
+      file.get(str);
     }
     for (int i = 0; i < word.length(); i++) {
       if (word[i] >= 65 && word[i] <= 90)
